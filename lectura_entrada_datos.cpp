76ba54bdb9bdea77ccc9fbca5ae91a9f9dd9f018
LEO22555 <lemartinezba@unal.edu.co>
2023-01-28 14:22:39 -0500

    entrada y lectura de datos

---
+++ lectura_entrada_datos.cpp
@@ -0,0 +1,14 @@
+//Lectura o entrada de datos
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	float numero; //definiendo la variable
+	cout<<"Digite un numero: "; 
+	cin>>numero; // guardando la variable
+	
+	cout<<"\n El numero que digito es: "<<numero;
+	
+	return 0;
+}
