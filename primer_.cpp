9e5febd88bbc914c34c93ed971d36bda0426630b
LEO22555 <lemartinezba@unal.edu.co>
2023-01-28 11:52:33 -0500

    primeros pasos

---
+++ primer_.cpp
@@ -0,0 +1,12 @@
+//Primer programa C++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Hola Leo"<<endl;
+
+    return 0;
+}
---
+++ tipos_de_datos.cpp
@@ -0,0 +1,13 @@
+// Tipos de datos básicos en C++
+#include<iostream>
+using namespace std;
+int main(){
+	int entero = 15;
+	float flotante = 3.1416;
+	double mayor = 16.34569874;
+	char letra = 'Leo55';
+	
+	cout<<letra;
+	return 0;
+}
+
