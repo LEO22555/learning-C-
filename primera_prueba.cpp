2fb94503aa5a16953536b38ff6e43ae609867042
LEO22555 <lemartinezba@unal.edu.co>
2023-01-28 14:46:22 -0500

    primera prueba

---
+++ primera_prueba.cpp
@@ -0,0 +1,18 @@
+/*primera prueba: Escribe un programa que lea la entrada estandar el precio de un producto
+y muestre en la salida estandar el precio del producto al aplicarle IVA
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n1, niva, iva=0;
+	
+	cout<<"digita el precio sin iva: "; cin>>n1;
+	
+	niva = n1+n1*.19;
+	
+	cout<<"El precio con iva es: "<<niva<<endl;
+	return 0;
+}
