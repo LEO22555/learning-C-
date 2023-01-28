1c0728c88c51d8e6b14e27c2d885e8f98339ba0f
LEO22555 <lemartinezba@unal.edu.co>
2023-01-28 14:33:42 -0500

    primer reto

---
+++ primer_reto.cpp
@@ -0,0 +1,24 @@
+/* Escribe un programa que lea la entrada estandar 2 numeros y muestre en la salida estandar
+su suma, resta, multiplicacion y division */
+
+#include<iostream>
+
+using namespace std;
+int main(){
+	int n1,n2, suma = 0, resta= 0, multiplicacion=0, division=0;
+	
+	cout<<"Digite un numero: "; cin>>n1;
+	cout<<"Digite otro numero: "; cin>>n2;
+	
+	suma = n1 + n2;
+	resta = n1 - n2;
+	multiplicacion = n1 * n2;
+	division = n1 / n2;
+	
+	cout<<"\La suma es: "<<suma<<endl;
+	cout<<"La resta es: "<<resta<<endl;
+	cout<<"La multiplicacion es: "<<multiplicacion<<endl;
+	cout<<"La division es: "<<division<<endl;
+		
+	return 0;
+}
