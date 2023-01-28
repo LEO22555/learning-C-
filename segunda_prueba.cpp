77c1b027d180f53c4eb24203113db93e437c0104
LEO22555 <lemartinezba@unal.edu.co>
2023-01-28 15:21:14 -0500

    segunda prueba

---
+++ segunda_prueba.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int sexo;
+	float nombre;
+	char altura;
+	
+	cout<<"Digita el sexo: "; cin>>sexo;
+	cout<<"Digita el nombre: "; cin>>nombre;
+	cout<<"Digita la altura: "; cin>>altura;
+	
+	cout<<"\nl El sexo es: "; <<sexo<<endl;
+	cout<<"El nombre es: "; <<nombre<<endl;
+	cout<<"La altura es: "; <<altura<<endl;	
+	
+	return 0;
+}
+
+
